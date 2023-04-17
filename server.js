16b82220182e39a346cb1f61f8a5ccfdc6d81448
CaptSV <124159621+CaptSV@users.noreply.github.com>
2023-04-17 16:10:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-// app.set('views', path.join(__dirname, 'views')); <-TEST
 
 // disables strict query and records entered does not need to follow schema
 mongoose.set('strictQuery', false);
